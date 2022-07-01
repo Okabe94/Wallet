3170f8c7c277a39f2fe532c0b64ffec727f11bb7
Okabe94 <pipe9411@hotmail.com>
2022-06-30 20:51:18 -0500

    Working tests on update pending repository

--- app/src/main/java/com/example/wallet/feature_main/domain/model/UseCaseWrapper.kt
+++
@@ -1,6 +0,0 @@
-package com.example.wallet.feature_main.domain.model
-
-import com.example.wallet.core.domain.entity.Expense
-import com.example.wallet.feature_main.domain.time.Time
-
-data class UseCaseWrapper(val time: Time, val pending: List<Expense>)
--- app/src/test/java/com/example/wallet/feature_main/domain/usecase/GetPendingUpdateUseCaseTest.kt
+++ app/src/test/java/com/example/wallet/feature_main/domain/usecase/GetPendingUpdateUseCaseTest.kt
@@ -15,85 +15,111 @@ import org.junit.Before
 import org.junit.Test
 import java.time.Clock
 import java.time.Instant
+import java.time.ZoneId
 import java.time.ZoneOffset
+
+private const val MONTH1 = "2020-01-20T10:05:00Z"
+private const val MONTH2 = "2020-02-20T10:05:00Z"
+private const val MONTH3 = "2020-03-20T10:05:00Z"
 
 class GetPendingUpdateUseCaseTest {
 
-    private lateinit var fakeTimeProvider: TimeProvider
     private lateinit var fakeTimeComparator: TimeComparator
     private lateinit var fakeRepository: RecurrentRepository
     private lateinit var fakePreferences: ApplicationPreferences
-    private lateinit var getPendingUpdateUseCase: GetPendingUpdateUseCase
 
     @Before
     fun setUp() {
         fakePreferences = FakeApplicationPreferences()
-        fakeTimeComparator = FakeTimeComparator(2, 2)
+        fakeTimeComparator = FakeTimeComparator(0, 0)
+        fakeRepository = FakeRecurrentRepository(createDummyData())
+    }
 
-        val auxTimeProvider = DefaultTimeProvider(
-            Clock.fixed(Instant.parse("2020-01-20T10:05:00Z"), ZoneOffset.UTC)
+    @Test
+    fun `Months elapsed - No preferences, get expenses`(): Unit = runBlocking {
+        val timeProvider = createFakeTimeProvider(MONTH3)
+        val useCase = createGetPendingUpdateUseCase(timeProvider)
+
+        val response = useCase()
+        assertThat(response).isNotNull()
+        assertThat(response).isNotEmpty()
+    }
+
+    @Test
+    fun `No month elapsed - No preferences, get null`(): Unit = runBlocking {
+        val timeProvider = createFakeTimeProvider(MONTH1)
+        val useCase = createGetPendingUpdateUseCase(timeProvider)
+
+        val response = useCase()
+        assertThat(response).isNull()
+    }
+
+    @Test
+    fun `No month elapsed - With preferences, get null`(): Unit = runBlocking {
+        val timeProvider = createFakeTimeProvider(MONTH1)
+        val useCase = createGetPendingUpdateUseCase(
+            timeProvider,
+            preferences = FakeApplicationPreferences(timeProvider.now().millis())
         )
+        val response = useCase()
+        assertThat(response).isNull()
+    }
 
-        val currentDate = auxTimeProvider.now()
-        val todayMillis = currentDate.millis()
-        val nextMonthMillis = currentDate.rollMonths().millis()
+    @Test
+    fun `Months elapsed - With preferences, get expenses`(): Unit =
+        runBlocking {
+            val timeProvider = createFakeTimeProvider(MONTH3)
+            val useCase = createGetPendingUpdateUseCase(
+                timeProvider,
+                preferences = FakeApplicationPreferences(timeProvider.now().millis()),
+                comparator = FakeTimeComparator(2)
+            )
+            val response = useCase()
+            assertThat(response).isNotNull()
+            assertThat(response).isNotEmpty()
+        }
 
+    @Test
+    fun `Getting No expenses, different day from last in preferences, successful`(): Unit =
+        runBlocking {
+            val timeProvider = createFakeTimeProvider(MONTH3)
+            val useCase = createGetPendingUpdateUseCase(
+                timeProvider,
+                preferences = FakeApplicationPreferences(timeProvider.now().millis())
+            )
+            val response = useCase()
+            assertThat(response).isNull()
+        }
+
+    private fun createFakeTimeProvider(date: String) = DefaultTimeProvider(
+        Clock.fixed(Instant.parse(date), ZoneOffset.UTC)
+    )
+
+    private fun createGetPendingUpdateUseCase(
+        timeProvider: TimeProvider,
+        repo: RecurrentRepository = fakeRepository,
+        preferences: ApplicationPreferences = fakePreferences,
+        comparator: TimeComparator = fakeTimeComparator,
+    ) = GetPendingUpdateUseCase(repo, preferences, timeProvider, comparator)
+
+    private fun createDummyData(): MutableList<Expense> {
+        val month1 = DefaultTimeProvider(
+            Clock.fixed(Instant.parse(MONTH1), ZoneId.systemDefault())
+        ).now().millis()
+        val month2 = DefaultTimeProvider(
+            Clock.fixed(Instant.parse(MONTH2), ZoneId.systemDefault())
+        ).now().millis()
+        val month3 = DefaultTimeProvider(
+            Clock.fixed(Instant.parse(MONTH3), ZoneId.systemDefault())
+        ).now().millis()
         val dataToInsert = mutableListOf<Expense>().apply {
-            add(Expense("monthly, current", "100", true, todayMillis, todayMillis))
-            add(Expense("no monthly, current", "100", false, todayMillis, todayMillis))
-            add(Expense("monthly, next month", "100", true, todayMillis, nextMonthMillis))
-            add(Expense("no monthly, next month", "100", false, todayMillis, nextMonthMillis))
+            add(Expense("monthly, current", "100", true, month1, month1))
+            add(Expense("no monthly, current", "100", false, month1, month1))
+            add(Expense("monthly, next month", "100", true, month1, month2))
+            add(Expense("no monthly, next month", "100", false, month1, month2))
+            add(Expense("monthly, next month", "100", true, month1, month3))
+            add(Expense("no monthly, next month", "100", false, month1, month3))
         }
-
-        fakeRepository = FakeRecurrentRepository(dataToInsert)
-    }
-
-    @Test
-    fun `Getting all that need update two months later, successful`(): Unit = runBlocking {
-        setUpProviderAndUseCase("2020-03-20T10:05:00Z")
-
-        val today = fakeTimeProvider.now()
-        val wrapper = getPendingUpdateUseCase()
-        assertThat(wrapper).isNotNull()
-        assertThat(wrapper?.pending).isNotEmpty()
-        println("========== ========== ========== ==========")
-        println("Today: ${today.format()}")
-        println("========== ========== ========== ==========")
-        wrapper?.pending?.forEach {
-            assertThat(it.isMonthly).isTrue()
-            assertThat(it.updatedUntil).isLessThan(today.millis())
-            println("Created: ${fakeTimeProvider.now(it.createdAt).format()}")
-            println("Update: ${fakeTimeProvider.now(it.updatedUntil).format()}")
-            println("========== ========== ========== ==========")
-        }
-    }
-
-    @Test
-    fun `Getting all that need update one month later, successful`(): Unit = runBlocking {
-        setUpProviderAndUseCase("2020-02-20T10:05:00Z")
-
-        val today = fakeTimeProvider.now()
-        val wrapper = getPendingUpdateUseCase()
-        assertThat(wrapper).isNotNull()
-        assertThat(wrapper?.pending).isNotEmpty()
-        assertThat(wrapper?.pending).hasSize(1)
-        println("========== ========== ========== ==========")
-        println("Today: ${today.format()}")
-        println("========== ========== ========== ==========")
-        wrapper?.pending?.forEach {
-            assertThat(it.isMonthly).isTrue()
-            assertThat(it.updatedUntil).isLessThan(today.millis())
-            println("Created: ${fakeTimeProvider.now(it.createdAt).format()}")
-            println("Update: ${fakeTimeProvider.now(it.updatedUntil).format()}")
-            println("========== ========== ========== ==========")
-        }
-    }
-
-    private fun setUpProviderAndUseCase(date: String) {
-        fakeTimeProvider = DefaultTimeProvider(Clock.fixed(Instant.parse(date), ZoneOffset.UTC))
-
-        getPendingUpdateUseCase = GetPendingUpdateUseCase(
-            fakeRepository, fakePreferences, fakeTimeProvider, fakeTimeComparator
-        )
+        return dataToInsert
     }
 }
